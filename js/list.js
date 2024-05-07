8a4d75b26b6fc7b744e7aec42f96b12a7ca1d11d
vincent <vinceshoter@gmail.com>
2024-05-07 10:48:48 +0200

    comiit

--- js/list.js
+++ js/list.js
@@ -4,12 +4,15 @@ function getList() {
     var div_primary = document.createElement('div')
     div_primary.classList.add('people-list')
     for (var i = 0; i < people.length; i += 1) {
+        var div_option = document.createElement('div')
+        var div_content = document.createElement('div')
         var newImage = document.createElement("img")
         newImage.setAttribute("src", "../icons/profile.png")
         newImage.setAttribute("alt", "profile_picture")
         var checkbox = document.createElement("input")
         checkbox.setAttribute("type", "checkbox")
         checkbox.setAttribute("class", "checkbox")
+        div_option.appendChild(checkbox)
         var div1 = document.createElement("div")
         var div1_1 = document.createElement("div")
         div1_1.setAttribute('class', 'flex')
@@ -21,13 +24,13 @@ function getList() {
         name1.setAttribute('class', 'name')
         phone1.innerHTML = 'numéro de téléphone'
         mail1.innerHTML = 'e-mail'
-        div1_1.appendChild(checkbox)
         div1_1.appendChild(newImage)
         div1_2.append(name1, phone1, mail1)
         div1.append(div1_1, div1_2)
         div1.setAttribute('class', 'flex row is-person')
         div1_2.setAttribute('class', 'flex column person-attribut')
         div1.addEventListener('click', showCV)
+        div_primary.appendChild(div_option)
         div_primary.appendChild(div1)
     }
     main.appendChild(div_primary)
