b919efeced5c6ce748955b4fb631f0dcbaa2f658
Abderrahman <abde.foull@gmail.com>
2024-05-27 13:55:25 +0200

    changement connexionBDD

--- php/Connexion.php
+++ php/Connexion.php
@@ -1,6 +1,6 @@
 <?php
-error_reporting(E_ALL); ini_set('display_errors', 1);
-// adresse serveur SQL
+
+// Adresse du serveur SQL
 $host = 'localhost';
 // Nom d'utilisateur MySQL
 $utilisateur = 'webProjet';
@@ -15,36 +15,50 @@ $connexion = new mysqli($host, $utilisateur, $mdp, $base_de_donnees);
 // Vérifier la connexion
 if ($connexion->connect_error) {
     die("Échec de la connexion : " . $connexion->connect_error);
-} else {
-    echo "Connexion réussie !";
 }
 
-// Récupère les valeurs du formulaire
+// Récupérer les valeurs du formulaire
 $username = $_POST["User"];
 $password = $_POST["Pass"];
 
-// Requête SQL pour récupérer le mot de passe associé à l'utilisateur donné
+// Requête SQL pour récupérer le nom d'utilisateur et le mot de passe associé à l'utilisateur donné
 $sql = "SELECT Username, Mdp 
         FROM utilisateurs AS us
-        INNER JOIN authentification AS au ON us.Id_MDP = au.Id_Mdp";
+        INNER JOIN authentification AS au ON us.Id_MDP = au.Id_Mdp 
+        WHERE Username = ?";
+$stmt = $connexion->prepare($sql);
 
-$resultat = $connexion->query($sql);
+if ($stmt) {
+    // Lier les paramètres
+    $stmt->bind_param("s", $username);
 
-if ($resultat->num_rows > 0) {
-    // Si l'utilisateur est trouvé dans la base de données
-    $row = $resultat->fetch_assoc();
-    $mdpBd = $row["Mdp"];
-    
-    // Vérifier si le mot de passe saisi correspond au mot de passe dans la base de données
-    if ($password == $mdpBd) {
-        // Rediriger vers la page gestionnaire.html
-        header("Location: gestionnaire.html");
-        exit; // Assurez-vous de terminer le script ici pour éviter toute exécution supplémentaire
+    // Exécuter la requête
+    $stmt->execute();
+
+    // Récupérer le résultat
+    $resultat = $stmt->get_result();
+
+    if ($resultat->num_rows == 1) {
+        // Si l'utilisateur est trouvé dans la base de données
+        $row = $resultat->fetch_assoc();
+        $mdpBd = $row["Mdp"];
+        
+        // Vérifier si le mot de passe saisi correspond au mot de passe dans la base de données
+        if ($password == $mdpBd) {
+            // Rediriger vers la page gestionnaire.html
+            header("Location: ../html/gestionnaire.html");
+            exit;
+        } else {
+            echo "Mot de passe incorrect.";
+        }
     } else {
-        echo "Mot de passe incorrect.";
+        echo "Utilisateur non trouvé.";
     }
+
+    // Fermer la requête préparée
+    $stmt->close();
 } else {
-    echo "Utilisateur non trouvé.";
+    echo "Erreur de préparation de la requête.";
 }
 
 // Fermer la connexion
